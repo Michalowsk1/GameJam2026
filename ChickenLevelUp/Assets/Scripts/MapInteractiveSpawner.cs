abfb9e758fbfe91cc91c68edf2ba3a4b22f1e013
Michalowsk1 <23619282@stu.mmu.ac.uk>
2026-01-22 15:38:21 +0000

    function to spawn objects away from eachother somewhat works

--- ChickenLevelUp/Assets/Scripts/MapInteractiveSpawner.cs
+++ ChickenLevelUp/Assets/Scripts/MapInteractiveSpawner.cs
@@ -1,39 +1,63 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class MapInteractiveSpawner : MonoBehaviour
 {
     public Vector2 flowerSpawn;
-    List<Vector2> flowerPositions = new List<Vector2>();
+    int flowerCount;
+    public List<Vector2> tempFlowerPositions = new List<Vector2>();
+    public List<Vector2> confirmedFlowerPositions = new List<Vector2>();
     [SerializeField] GameObject[] flowers;
     [SerializeField] GameObject[] nests;
     void Start()
     {
-        for(int i = 0; i < 25; i++)
-        {
-            flowerSpawn = new Vector2(Random.Range(-20, 20), Random.Range(-12, 7));
-            GameObject flowerPrefab = Instantiate(flowers[Random.Range(0,flowers.Length)], flowerSpawn, Quaternion.identity);
-            flowerPositions.Add(flowerSpawn);
-
-            foreach(Vector2 position in flowerPositions)
-            {
-                if (Vector2.Distance(position, flowerSpawn) < 2)
-                {
-                    Destroy(flowerPrefab);
-                    i--;
-                }
-            }
-        }
+        flowerGenerator();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (confirmedFlowerPositions.Count == 10)
+            flowerSpawner();
     }
 
-    void FlowerSpawner()
+    void flowerGenerator()
     {
+        Vector2 tempPos = GenerateFlowerPosition();
+        if (tempFlowerPositions.Count < 100)
+        {
+            tempFlowerPositions.Add(tempPos);
+            flowerGenerator();
+        }
+        foreach (Vector2 pos in tempFlowerPositions)
+        {
+            if (Vector2.Distance(tempPos, pos) > 2 && confirmedFlowerPositions.Count < 10)
+            {
+                confirmedFlowerPositions.Add(pos);
+            }
+        }
 
+    }
+
+    void flowerSpawner()
+    {
+        int spawned = 0;
+        if (spawned < 10)
+        {
+            foreach (Vector2 pos in confirmedFlowerPositions)
+            {
+                Instantiate(flowers[Random.Range(0, flowers.Length)], pos, Quaternion.identity);
+                spawned++;
+            }
+        }
+    }
+
+
+    Vector2 GenerateFlowerPosition()
+    {
+        float yPos = Random.Range(-12, 7);
+        flowerSpawn = new Vector3(Random.Range(-20, 20), yPos,yPos);
+        return flowerSpawn;
     }
 }
